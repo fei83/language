958eebba906e5ee3050566d623003c5939fd30ca
m516 <m516@m516>
2015-10-31 00:58:21 +0800

    dfzgfdagdsgs

---
+++ language/wiki/migrations/0004_auto_20151031_0024.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('wiki', '0003_category_slug'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='category',
+            name='likes',
+            field=models.IntegerField(default=0),
+        ),
+        migrations.AlterField(
+            model_name='category',
+            name='slug',
+            field=models.SlugField(unique=True),
+        ),
+        migrations.AlterField(
+            model_name='category',
+            name='views',
+            field=models.IntegerField(default=0),
+        ),
+    ]
